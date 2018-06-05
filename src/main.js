27a69c53b6052cc4e14f80dbd04e1a06016a4f82
gsuxlzt <jami.carangan@gmail.com>
2018-06-05 11:46:57 +0800

    :recycle: refactor main and App files to handle routing.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import Search from './components/Search.vue';
+import User from './components/User.vue';
+import Repo from './components/Repo.vue';
+Vue.use(VueRouter);
 
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Search },
+    { path: '/:user', component: User },
+    { path: '/:user/:repo', component: Repo }
+  ]
+})
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
