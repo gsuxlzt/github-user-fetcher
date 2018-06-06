834b086ab47e70c289ab5dacef4acfc1aa1ca246
gsuxlzt <jami.carangan@gmail.com>
2018-06-06 10:15:05 +0800

    :tada: add PageNotFound component.

---
+++ src/components/404.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>It seems like your search didn't give any meaningful results. Why don't you go back and <router-link class="link" :to="{path: '/'}">search</router-link> again?</h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  padding-top: 50px;
+  margin: 0px auto;
+  font-size: 36px;
+  max-width: 55%;
+  text-align: justify;
+}
+</style>
