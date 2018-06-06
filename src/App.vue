302379087c6504ebd9312aa2faa09a48af37e311
gsuxlzt <jami.carangan@gmail.com>
2018-06-06 10:17:53 +0800

    :lipstick: add global styles.

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,25 @@
     <router-view></router-view>
   </div>
 </template>
+
+<style lang="scss">
+body {
+  background-color: #eee;
+  color: #24292f;
+  font-family: sans-serif;
+  padding: 5px 15px;
+}
+.link {
+  cursor: pointer;
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+  margin: 0 15px;
+  color: #007bff;
+  &:focus,
+  &:active,
+  &:hover {
+    text-decoration: underline;
+  }
+}
+</style>
+
